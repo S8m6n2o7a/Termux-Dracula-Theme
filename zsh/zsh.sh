5ef2450f143b0c600df8411b72ee00599ce47fd3
s8m6n2o7a <ijiwy45riw@gmail.com>
2021-10-02 19:16:10 +0530

    fixing bug

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-
 pkg update
 pkg install toilet
 toilet -f term -F border --gay "Hola, soy s8m6n2o7a mucho gusto"
 #Installing zsh
 pkg install zsh
 #Installing Oh-My-Zsh
-curl sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm ~/.zshrc
+cp .zshrc $HOME
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 #Installing Dracula theme
 git clone https://github.com/dracula/zsh.git
-ln -s $DRACULA_THEME/dracula.zsh-theme $OH_MY_ZSH/themes/dracula.zsh-theme
+cd zsh && cp dracula.zsh-theme ~/.oh-my-zsh/themes/
+cp -rf lib oh-my-zsh/themes
 toilet -f term -F border --gay "Mucho"
