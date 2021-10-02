2b8ee6feb53da439e1f477b603218349f48ff777
sumon <64481672+S8m6n2o7a@users.noreply.github.com>
2021-10-02 19:39:33 +0530

    Update vim.sh

--- vim/vim.sh
+++ vim/vim.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 pkg update
-pkg install toilet
 toilet -f term -F border --gay "Hola, soy s8m6n2o7a mucho gustov"
+sleep 3
 pkg install vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
