e916b4e1063cd85bf4c2e214d91faa4456b80cf4
sumon <64481672+S8m6n2o7a@users.noreply.github.com>
2021-10-02 19:38:41 +0530

    Update termux.sh

--- termux/termux.sh
+++ termux/termux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pkg update
-pkg install toilet
 toilet -f term -F border --gay "Hola, soy s8m6n2o7a mucho gusto"
+sleep 3
 git clone https://github.com/dracula/termux/
 cd termux
 cp colors.properties ~/.termux
