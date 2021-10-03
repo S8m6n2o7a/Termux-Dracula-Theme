faa9455b5a3fafec1cd299b5eda4a5e9026cdd76
sumon <64481672+S8m6n2o7a@users.noreply.github.com>
2021-10-03 10:36:21 +0530

    Update install.py

--- install.py
+++ install.py
@@ -2,14 +2,6 @@ import os
 
 os.system("pkg update")
 os.system("pkg install toilet")
-zsh = input("Do you want to install zsh (Y/n): ")
-if zsh == "n":
-    pass
-else:
-    os.system("cd zsh && bash zsh.sh")
-    os.system("cd ..")
-
-
 
 termux = input("Dracula theme for termux (Y/n): ")
 if termux == "n":
@@ -18,10 +10,17 @@ else:
     os.system("cd termux && bash termux.sh")
     os.system("cd ..")
 
-
+    
 vim = input("Do you want to install a amazing text editor with amazing theme (Y/n): ")
 if vim == "n":
     pass
 else:
     os.system("cd vim && bash vim.sh")
+    os.system("cd ..")
+    
+zsh = input("Do you want to install zsh (Y/n): ")
+if zsh == "n":
+    pass
+else:
+    os.system("cd zsh && bash zsh.sh")
     os.system("cd ..")
