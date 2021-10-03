c897fab4e00f868bb782b5da6936b3ffce75c471
sumon <64481672+S8m6n2o7a@users.noreply.github.com>
2021-10-03 11:27:15 +0530

    Update vim.sh

--- vim/vim.sh
+++ vim/vim.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 pkg update
+apt update --fix-missing
 toilet -f term -F border --gay "Hola, soy s8m6n2o7a mucho gustov"
 sleep 3
-pkg install vim
+pkg install -y vim
+apt update --fix-missing
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+apt update --fix-missing
 cp .vimrc $HOME
 mkdir ~/.vim/plug
+apt update --fix-missing
 toilet -f term -F border --gay "Mucho"
