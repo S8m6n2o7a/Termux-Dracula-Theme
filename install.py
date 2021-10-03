05b85753f1893e4e193bf7a3b4769698ef92ec5d
sumon <64481672+S8m6n2o7a@users.noreply.github.com>
2021-10-03 11:25:35 +0530

    Update install.py

--- install.py
+++ install.py
@@ -1,26 +1,37 @@
 import os
 
 os.system("pkg update")
-os.system("pkg install toilet")
+os.system("apt update --fix-missing")
+os.system("pkg install -y toilet")
+os.system("apt update --fix-missing")
 
 termux = input("Dracula theme for termux (Y/n): ")
 if termux == "n":
     pass
 else:
+    os.system("apt update --fix-missing")
     os.system("cd termux && bash termux.sh")
+    os.system("apt update --fix-missing")
     os.system("cd ..")
+    os.system("apt update --fix-missing")
 
     
 vim = input("Do you want to install a amazing text editor with amazing theme (Y/n): ")
 if vim == "n":
     pass
 else:
+    os.system("apt update --fix-missing")
     os.system("cd vim && bash vim.sh")
+    os.system("apt update --fix-missing")
     os.system("cd ..")
+    os.system("apt update --fix-missing")
     
 zsh = input("Do you want to install zsh (Y/n): ")
 if zsh == "n":
     pass
 else:
+    os.system("apt update --fix-missing")
     os.system("cd zsh && bash zsh.sh")
+    os.system("apt update --fix-missing")
     os.system("cd ..")
+    os.system("apt update --fix-missing")
