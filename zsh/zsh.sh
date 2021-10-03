f46d875fdb41710ecdd04ce39be90aa7b9808cbc
sumon <64481672+S8m6n2o7a@users.noreply.github.com>
2021-10-03 11:30:39 +0530

    Update zsh.sh

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -2,15 +2,21 @@
 
 pkg update
 toilet -f term -F border --gay "Hola, soy s8m6n2o7a mucho gusto"
+apt update --fix-missing
 sleep 3
 #Installing zsh
-pkg install zsh
+pkg install -y zsh
+apt update --fix-missing
 #Installing Oh-My-Zsh
 rm ~/.zshrc
 cp .zshrc $HOME
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+apt update --fix-missing
 #Installing Dracula theme
 git clone https://github.com/dracula/zsh.git
+apt update --fix-missing
 cd zsh && cp dracula.zsh-theme ~/.oh-my-zsh/themes/
-cp -rf lib oh-my-zsh/themes
+apt update --fix-missing
+cp -rf lib ~/.oh-my-zsh/themes
+apt update --fix-missing
 toilet -f term -F border --gay "Mucho"
